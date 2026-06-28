76706b279c126a32b6ee196f4add8165ef09970e
Amam17 <dauramansurahmad83@gmail.com>
2026-06-28 09:55:51 +0100

    Add Supabase configuration to env.js

---
+++ config/env.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL =
+"https://gzrybqpaoakjmmaftqri.supabase.co";
+
+const SUPABASE_KEY =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6cnlicXBhb2Fram1tYWZ0cXJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NTI3MzYsImV4cCI6MjA5NzUyODczNn0.11nB7gouACiMPxUCKM3Hpgr78HDO6uB0BRrtDaBXD8U";
