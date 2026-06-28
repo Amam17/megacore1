c716c0698318742d4d6d10bdb89df74cccbf8b7a
Amam17 <dauramansurahmad83@gmail.com>
2026-06-28 10:00:16 +0100

    Initialize Supabase client in supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,15 @@
+import {
+createClient
+}
+
+from
+
+'https://esm.sh/@supabase/supabase-js'
+
+const supabase =
+createClient(
+https://gzrybqpaoakjmmaftqri.supabase.co,
+sb_publishable_hOy5oRqTebovu-noQ8LnOA_BlnLaCnf
+)
+
+export default supabase
